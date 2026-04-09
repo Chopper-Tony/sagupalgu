6f53311e2823c6631b310e16bcb519ecbfb0df02
Chopper-Tony <hi.hello.bonjour.ciao@gmail.com>
2026-04-09 11:33:45 +0900

    fix: 익스텐션 서버 URL 18.232.188.74로 갱신

--- sagupalgu-extension/background.js
+++ sagupalgu-extension/background.js
@@ -6,7 +6,7 @@
 importScripts("cookies.js");
 
 // 기본 서버 URL (배포 시 변경)
-const DEFAULT_SERVER_URL = "http://3.80.231.49";
+const DEFAULT_SERVER_URL = "http://18.232.188.74";
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "CONNECT_PLATFORM") {
